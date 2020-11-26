4bb20de3e460e93c11e6c70b28a5d0c824ac6d56
Oleja <sooperoleja@gmail.com>
2020-11-26 14:57:41 +0500

    added filters and permissions

--- orders/filters.py
+++ orders/filters.py
@@ -1,9 +1,19 @@
 from django_filters import rest_framework as filters
 from orders.models import Order
+from shop.models import Product
 
 
 class OrderFilter(filters.FilterSet):
+    min_price = filters.NumberFilter(field_name='order_price', lookup_expr='gte')
+    max_price = filters.NumberFilter(field_name='order_price', lookup_expr='lte')
+    created_at = filters.DateFromToRangeFilter()
+    updated_at = filters.DateFromToRangeFilter()
+    positions = filters.ModelChoiceFilter(
+        field_name="products",
+        to_field_name="id",
+        queryset=Product.objects.all(),
+    )
 
     class Meta:
         model = Order
-        fields = ('status', 'amount', 'created_at', 'updated_at', 'positions',)
+        fields = ('status', 'min_price', 'max_price', 'created_at', 'updated_at', 'positions',)
--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,8 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.exceptions import ValidationError
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.viewsets import ModelViewSet
+from orders.filters import OrderFilter
 from orders.models import Order
 from orders.serializers import OrderSerializer
 
@@ -6,3 +10,20 @@ from orders.serializers import OrderSerializer
 class OrderViewSet(ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = OrderFilter
+
+    def get_queryset(self):
+        if self.request.user.is_superuser:
+            return Order.objects.prefetch_related('positions').all()
+        if self.request.user.is_authenticated:
+            return Order.objects.prefetch_related('positions').filter(creator=self.request.user.id)
+        else:
+            raise ValidationError({"Error": "Вы не авторизированы!"})
+
+    def get_permissions(self):
+        if self.action == 'create':
+            return [IsAuthenticated()]
+        if self.action in ['update', 'partial_update', 'destroy']:
+            return [IsAdminUser()]
+        return []
